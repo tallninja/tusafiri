7dd0880528635d37be1c16a238777d038b08ae3e
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-22 14:33:12 +0300

    Indexed the idNo and employeeId fields

--- hr-management/src/models/Employee.js
+++ hr-management/src/models/Employee.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const EmployeeSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  idNo: { type: Number, required: true, unique: true },
+  idNo: { type: Number, required: true, unique: true, index: true },
   email: { type: String, required: true },
   phoneNumber: { type: String, required: true },
-  employeeId: { type: String, required: true, unique: true },
+  employeeId: { type: String, required: true, unique: true, index: true },
   password: { type: String, required: true },
   role: { type: mongoose.Types.ObjectId, ref: 'roles' },
   createdAt: { type: Date },
