52ad21ba91aeb258dd87c7a3caa4ac0850a6e9dc
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-26 09:07:05 +0300

    Added paid flag on a booking

--- booking-system/src/models/Booking.js
+++ booking-system/src/models/Booking.js
@@ -5,65 +5,66 @@ const Invoice = require('./Invoice');
 const Journey = require('./Journey');
 
 const BookingSchema = new mongoose.Schema({
-  journey: { type: mongoose.Types.ObjectId, ref: 'journeys', required: true },
-  seats: [{ type: mongoose.Types.ObjectId, ref: 'seats' }],
-  createdAt: { type: Date },
-  updatedAt: { type: Date },
+	journey: { type: mongoose.Types.ObjectId, ref: 'journeys', required: true },
+	seats: [{ type: mongoose.Types.ObjectId, ref: 'seats' }],
+	createdAt: { type: Date },
+	updatedAt: { type: Date },
+	paid: { type: Boolean, default: false },
 });
 
 BookingSchema.pre('save', async function (next) {
-  try {
-    Invoice.generateInvoice(this);
-    let journey = await Journey.findById(this.journey).exec();
-    journey.bookedSeats = _.unionBy(
-      journey.bookedSeats,
-      this.seats,
-      JSON.stringify
-    );
-    journey.save();
-    next();
-  } catch (err) {
-    throw err;
-  }
+	try {
+		Invoice.generateInvoice(this);
+		let journey = await Journey.findById(this.journey).exec();
+		journey.bookedSeats = _.unionBy(
+			journey.bookedSeats,
+			this.seats,
+			JSON.stringify
+		);
+		journey.save();
+		next();
+	} catch (err) {
+		throw err;
+	}
 });
 
 BookingSchema.pre('updateOne', async function (next) {
-  try {
-    let booking = await this.model.findOne(this.getQuery()).exec();
-    let invoice = await Invoice.findOne({ booking: booking._id }).exec();
-    Invoice.updateInvoice(invoice, booking);
-    next();
-  } catch (err) {
-    throw err;
-  }
+	try {
+		let booking = await this.model.findOne(this.getQuery()).exec();
+		let invoice = await Invoice.findOne({ booking: booking._id }).exec();
+		Invoice.updateInvoice(invoice, booking);
+		next();
+	} catch (err) {
+		throw err;
+	}
 });
 
 BookingSchema.post('updateOne', async function () {
-  try {
-    let booking = await this.model.findOne(this.getQuery()).exec();
-    await booking.save();
-  } catch (err) {
-    throw err;
-  }
+	try {
+		let booking = await this.model.findOne(this.getQuery()).exec();
+		await booking.save();
+	} catch (err) {
+		throw err;
+	}
 });
 
 BookingSchema.pre(
-  'deleteOne',
-  { document: true, query: false },
-  async function (next) {
-    try {
-      let journey = await Journey.findById(this.journey).exec();
-      let bookedSeats = this.seats.map((seat) => JSON.stringify(seat._id));
-      journey.bookedSeats = _.remove(
-        journey.bookedSeats,
-        (seat) => !bookedSeats.includes(JSON.stringify(seat))
-      );
-      await journey.save();
-      next();
-    } catch (err) {
-      throw err;
-    }
-  }
+	'deleteOne',
+	{ document: true, query: false },
+	async function (next) {
+		try {
+			let journey = await Journey.findById(this.journey).exec();
+			let bookedSeats = this.seats.map((seat) => JSON.stringify(seat._id));
+			journey.bookedSeats = _.remove(
+				journey.bookedSeats,
+				(seat) => !bookedSeats.includes(JSON.stringify(seat))
+			);
+			await journey.save();
+			next();
+		} catch (err) {
+			throw err;
+		}
+	}
 );
 
 module.exports = mongoose.model('bookings', BookingSchema);
--- booking-system/src/models/index.js
+++ booking-system/src/models/index.js
@@ -7,44 +7,46 @@ const Booking = require('./Booking');
 const Invoice = require('./Invoice');
 
 const connectToDb = () => {
-  mongoose.connect(db.MONGO_URI, (err) => {
-    if (err) {
-      console.log('Error:', err);
-    } else {
-      console.log('Info:', 'Conneted to database.');
+	mongoose.connect(db.MONGO_URI, (err) => {
+		if (err) {
+			console.log('Error:', err);
+		} else {
+			console.log('Info:', 'Conneted to database.');
 
-      cron.schedule('*/2 * * * *', async () => {
-        // runs after every 2 minutes
-        console.log('Info:', 'Cleaning the bookings collection...');
-        try {
-          let bookings = await Booking.find().exec();
+			cron.schedule('*/2 * * * *', async () => {
+				// runs after every 2 minutes
+				console.log('Info:', 'Cleaning the bookings collection...');
+				try {
+					let bookings = await Booking.find({ paid: false }).exec(); // find all bookings which have not been paid for
 
-          bookings.map(async (booking) => {
-            let invoice = await Invoice.findOne({
-              booking: booking._id,
-            }).exec();
+					if (bookings.length > 0) {
+						bookings.map(async (booking) => {
+							let invoice = await Invoice.findOne({
+								booking: booking._id,
+							}).exec();
 
-            if (!invoice) {
-              // check if booking is missing an invoice and is not paid for
-              await booking.deleteOne();
-              console.log('Info:', `Booking ${booking._id} was deleted.`);
-            }
-          });
-        } catch (err) {
-          console.log('Error:', err);
-        }
-      });
-    }
-  });
+							if (!invoice) {
+								// check if booking is missing an invoice and is not paid for
+								await booking.deleteOne();
+								console.log('Info:', `Booking ${booking._id} was deleted.`);
+							}
+						});
+					}
+				} catch (err) {
+					console.log('Error:', err);
+				}
+			});
+		}
+	});
 };
 
 module.exports = {
-  connectToDb: connectToDb,
-  Booking: require('./Booking'),
-  Invoice: require('./Invoice'),
-  Ticket: require('./Ticket'),
-  Payment: require('./Payment'),
-  Journey: require('./Journey'),
-  Bus: require('./Bus'),
-  Seat: require('./Seat'),
+	connectToDb: connectToDb,
+	Booking: require('./Booking'),
+	Invoice: require('./Invoice'),
+	Ticket: require('./Ticket'),
+	Payment: require('./Payment'),
+	Journey: require('./Journey'),
+	Bus: require('./Bus'),
+	Seat: require('./Seat'),
 };
