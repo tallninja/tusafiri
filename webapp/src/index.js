3b849553433216a60c522f6f5927f68ae55da2ca
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-08 11:53:49 +0300

    Started on the webapp

---
+++ webapp/src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
