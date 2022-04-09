32f14ae52e14eb00d4d72c311929c7f83fa19cc1
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-09 09:12:24 +0300

    Destructured the card components

--- webapp/src/components/Footer.js
+++ webapp/src/components/Footer.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
-		<div className='container'>
-			<footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
-				<div className='col-md-4 d-flex align-items-center'>
+		<div className='container footer'>
+			<footer className='d-flex justify-content-between align-items-center p-3 my-4 border-top'>
+				<div className='col-md-4 d-flex align-items-center align-content-center'>
 					<Link
 						to='/'
 						className='mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1'
