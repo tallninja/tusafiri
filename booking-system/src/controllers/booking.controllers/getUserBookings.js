989d3d998ac11de3d94e4b5a9564387e0bd27fba
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-20 13:51:42 +0300

    Minor commit

---
+++ booking-system/src/controllers/booking.controllers/getUserBookings.js
@@ -0,0 +1,37 @@
+const { StatusCodes: Sc } = require('http-status-codes');
+
+const { Booking } = require('../../models');
+
+const handleError = (err, res) => {
+	console.log('Error:', err);
+	return res.status(Sc.INTERNAL_SERVER_ERROR).json(err);
+};
+
+module.exports = async (req, res) => {
+	const user = req.headers['x-user'];
+	try {
+		const userBookings = await Booking.find({ user })
+			.sort('-craeatedAt')
+			.populate([
+				{ path: 'seats', select: 'number -_id' },
+				{
+					path: 'journey',
+					select: 'route fare departureTime',
+					populate: [
+						{
+							path: 'route',
+							model: 'routes',
+							populate: [
+								{ path: 'from', model: 'locations' },
+								{ path: 'to', model: 'locations' },
+							],
+						},
+					],
+				},
+			])
+			.exec();
+		return res.status(Sc.OK).json(userBookings);
+	} catch (err) {
+		return handleError(err, res);
+	}
+};
